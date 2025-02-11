2eb30b7c101cc2329bc894fe9e4aea3464370249
Samuel Monteiro <samuel.dvmonteiro@gmail.com>
2025-02-10 21:04:25 -0300

    add new project

--- src/pages/Home/Projects/index.jsx
+++ src/pages/Home/Projects/index.jsx
@@ -2,12 +2,29 @@ import React from 'react'
 import PImage1 from "../../../assets/images/projects/1.png"
 import PImage2 from "../../../assets/images/projects/2.png"
 import PImage3 from "../../../assets/images/projects/3.png"
-import PImage4 from "../../../assets/images/projects/4.png"
+import PImage6 from "../../../assets/images/projects/6.png"
 import PImage5 from "../../../assets/images/projects/5.png"
 import ProjectItem from './ProjectItem'
 
 
 const projects = [
+  {
+    image: PImage6,
+    title: 'Painel Administrativo Completo',
+    text: 'Painel administrativo completo para a gestão e controle financeiro de igrejas. Com ele, é possível registrar e acompanhar dízimos, ofertas e despesas de forma organizada, além de gerenciar membros e congregações de maneira eficiente. O sistema também conta com um painel intuitivo para a administração monitorar o fluxo de caixa, gerar relatórios detalhados e garantir total transparência na gestão financeira da igreja.',
+    tags: ['Symfony', 'ReactJS', 'Docker'],
+    accessLink: 'https://adcampodaponte.site/',
+    codeLink: 'https://github.com/samueldmonteiro/sistema-gestao-igreja'
+  },
+
+  {
+    image: PImage1,
+    title: 'Escola Técnica',
+    text: 'Sistema completo desenvolvido para uma escola técnica. Com ele, os alunos podem se inscrever online nos cursos, realizar o pagamento com confirmação imediata, e acompanhar todo o processo. Além disso, o sistema inclui um painel de controle para que a administração possa gerenciar facilmente as informações do site e as inscrições dos alunos.',
+    tags: ['PHP', 'Laravel', 'JavaScript'],
+    accessLink: 'https://institutoiep.net.br/',
+  },
+
   {
     image: PImage2,
     title: 'Páginas de Entregas',
@@ -15,14 +32,6 @@ const projects = [
     tags: ['HTML', 'CSS', 'JavaScript'],
     accessLink: 'https://entregas-app.netlify.app/',
     codeLink: 'https://github.com/samueldmonteiro/Entregas'
-  },
-  {
-    image: PImage3,
-    title: 'Clone do StarBucks',
-    text: 'Esta homepage é uma simulação fiel da página do Starbucks, destacando conhecimentos em design, estilo e organização de elementos na tela para proporcionar uma experiência visual atraente e intuitiva.',
-    tags: ['HTML', 'CSS', 'JavaScript'],
-    accessLink: 'https://starb-home.netlify.app/',
-    codeLink: 'https://github.com/samueldmonteiro/starbucks-homepage'
   },
 
   {
@@ -33,13 +42,13 @@ const projects = [
     accessLink: 'https://buy-pizza-app.netlify.app/',
     codeLink: 'https://github.com/samueldmonteiro/buy-pizza'
   },
-
   {
-    image: PImage1,
-    title: 'Escola Técnica',
-    text: 'Sistema completo desenvolvido para uma escola técnica. Com ele, os alunos podem se inscrever online nos cursos, realizar o pagamento com confirmação imediata, e acompanhar todo o processo. Além disso, o sistema inclui um painel de controle para que a administração possa gerenciar facilmente as informações do site e as inscrições dos alunos.',
-    tags: ['PHP', 'Laravel', 'JavaScript'],
-    accessLink: 'https://institutoiep.net.br/',
+    image: PImage3,
+    title: 'Clone do StarBucks',
+    text: 'Esta homepage é uma simulação fiel da página do Starbucks, destacando conhecimentos em design, estilo e organização de elementos na tela para proporcionar uma experiência visual atraente e intuitiva.',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    accessLink: 'https://starb-home.netlify.app/',
+    codeLink: 'https://github.com/samueldmonteiro/starbucks-homepage'
   },
 ];
 
